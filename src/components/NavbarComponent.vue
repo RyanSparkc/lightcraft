02a50474e0c0aef6ca7be2d969808a3110389c08
forest079 <bluegx08@gmail.com>
2025-04-14 12:17:54 +0800

    fix(ui): 修正導覽列在手機版上的收合行為 🐛

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -2,29 +2,36 @@
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
       <RouterLink class="navbar-brand" to="/">Logo</RouterLink>
-      <button class="navbar-toggler" type="button"
-       data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <RouterLink class="nav-link" to="/about">關於我們</RouterLink>
-        </li>
-        <li class="nav-item">
-          <RouterLink class="nav-link" to="/products">產品列表</RouterLink>
-        </li>
-        <li class="nav-item">
-          <RouterLink class="nav-link" to="/cart">購物車</RouterLink>
-        </li>
-      </ul>
-      <div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/about">關於我們</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/products">產品列表</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/cart">購物車</RouterLink>
+          </li>
+        </ul>
         <ul class="navbar-nav">
           <li class="nav-item">
-            <RouterLink class="btn btn-outline-primary position-relative"
-             to="/"><i class="bi bi-cart"></i>
-              <span class="position-absolute top-0 start-100 translate-middle
-               badge rounded-pill bg-danger">
+            <RouterLink class="btn btn-outline-primary position-relative" to="/"
+              ><i class="bi bi-cart"></i>
+              <span
+                class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+              >
                 {{ carts?.length }}
                 <span class="visually-hidden">unread messages</span>
               </span>
@@ -34,7 +41,9 @@
             <RouterLink class="nav-link" to="/login">登入</RouterLink>
           </li>
           <li class="nav-item">
-            <RouterLink class="nav-link" to="/admin/products">後台 - 購物車列表</RouterLink>
+            <RouterLink class="nav-link" to="/admin/products"
+              >後台 - 購物車列表</RouterLink
+            >
           </li>
         </ul>
       </div>
--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -1,30 +1,24 @@
 <template>
   <div class="position-relative">
-    <div class="position-absolute" style="top:0; bottom:0; left:0; right:0; background-image: url(https://images.unsplash.com/photo-1480399129128-2066acb5009e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80);
-      background-position: center center; opacity: 0.1;"></div>
-    <div class="container d-flex flex-column" style="min-height: 100vh;">
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="./index.html">Navbar</a>
-        <button class="navbar-toggler" type="button"
-         data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-item nav-link me-4 active"
-             href="./index.html">Home <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link me-4" href="./product.html">Product</a>
-            <a class="nav-item nav-link me-4" href="./detail.html">Detail</a>
-            <a class="nav-item nav-link" href="./cart.html"><i class="fas fa-shopping-cart"></i></a>
-          </div>
-        </div>
-      </nav>
+    <div
+      class="position-absolute"
+      style="
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-image: url(https://images.unsplash.com/photo-1480399129128-2066acb5009e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80);
+        background-position: center center;
+        opacity: 0.1;
+      "
+    ></div>
+    <div class="container d-flex flex-column" style="min-height: 100vh">
       <div class="row justify-content-center my-auto">
         <div class="col-md-4 text-center">
           <h2>Lorem ipsum.</h2>
-          <p class="text-muted mb-0">Lorem ipsum dolor
-             sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.
+          <p class="text-muted mb-0">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod.
           </p>
           <button class="btn btn-dark rounded-0 mt-6">Lorem ipsum.</button>
         </div>
@@ -37,7 +31,9 @@
         <div class="card border-0 mb-4">
           <img
             src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-            class="card-img-top rounded-0" alt="..." />
+            class="card-img-top rounded-0"
+            alt="..."
+          />
           <div class="card-body text-center">
             <h4>Lorem ipsum</h4>
             <div class="d-flex justify-content-between">
@@ -53,7 +49,9 @@
         <div class="card border-0 mb-4">
           <img
             src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-            class="card-img-top rounded-0" alt="..." />
+            class="card-img-top rounded-0"
+            alt="..."
+          />
           <div class="card-body text-center">
             <h4>Lorem ipsum</h4>
             <div class="d-flex justify-content-between">
@@ -69,7 +67,9 @@
         <div class="card border-0 mb-4">
           <img
             src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-            class="card-img-top rounded-0" alt="..." />
+            class="card-img-top rounded-0"
+            alt="..."
+          />
           <div class="card-body text-center">
             <h4>Lorem ipsum</h4>
             <div class="d-flex justify-content-between">
@@ -85,15 +85,21 @@
   </div>
   <div class="bg-light mt-7">
     <div class="container">
-      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div
+        id="carouselExampleControls"
+        class="carousel slide"
+        data-ride="carousel"
+      >
         <div class="carousel-inner">
           <div class="carousel-item active">
             <div class="row justify-content-center py-7">
               <div class="col-md-6 text-center">
                 <h3>Lorem ipsum.</h3>
-                <p class="my-5">“Lorem ipsum dolor sit amet, consetetur
-                  sadipscing elitr, sed diam nonumy eirmod tempor
-                  invidunt ut labore et dolore magna aliquyam erat.”</p>
+                <p class="my-5">
+                  “Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat.”
+                </p>
                 <p><small>—Lorem ipsum dolor sit amet.—</small></p>
               </div>
             </div>
@@ -102,9 +108,11 @@
             <div class="row justify-content-center py-7">
               <div class="col-md-6 text-center">
                 <h3>Lorem ipsum.</h3>
-                <p class="my-5">“Lorem ipsum dolor sit amet, consetetur
-                  sadipscing elitr, sed diam nonumy eirmod tempor
-                  invidunt ut labore et dolore magna aliquyam erat.”</p>
+                <p class="my-5">
+                  “Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat.”
+                </p>
                 <p><small>—Lorem ipsum dolor sit amet.—</small></p>
               </div>
             </div>
@@ -113,21 +121,31 @@
             <div class="row justify-content-center py-7">
               <div class="col-md-6 text-center">
                 <h3>Lorem ipsum.</h3>
-                <p class="my-5">“Lorem ipsum dolor sit amet, consetetur
-                  sadipscing elitr, sed diam nonumy eirmod tempor
-                  invidunt ut labore et dolore magna aliquyam erat.”</p>
+                <p class="my-5">
+                  “Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                  aliquyam erat.”
+                </p>
                 <p><small>—Lorem ipsum dolor sit amet.—</small></p>
               </div>
             </div>
           </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls"
-         role="button" data-slide="prev">
+        <a
+          class="carousel-control-prev"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="prev"
+        >
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleControls"
-         role="button" data-slide="next">
+        <a
+          class="carousel-control-next"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="next"
+        >
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
@@ -137,7 +155,6 @@
 </template>
 
 <script>
-
 export default {
   data() {
     return {
@@ -152,5 +169,4 @@ export default {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
