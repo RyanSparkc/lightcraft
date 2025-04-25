0653c87188f8c657b04afefcaeab63cc7fa8b63c
forest079 <bluegx08@gmail.com>
2025-04-25 16:53:14 +0800

    Updates

--- assets/modalMixin-0r3r6NdI.js
+++
@@ -1,1 +0,0 @@
-import{M as o}from"./bootstrap.esm-TTjMTX3M.js";const d={methods:{openModal(){this.modal.show()},closeModal(){this.modal.hide()}},mounted(){this.modal=new o(this.$refs.modal,{backdrop:"static",keyboard:!1})}};export{d as m};
