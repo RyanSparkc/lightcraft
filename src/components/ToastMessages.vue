7f5e1a850319cf23a1669903eb49f293330950d5
forest079 <bluegx08@gmail.com>
2025-06-12 18:18:29 +0800

    refactor(ToastMessages): 調整 ToastMessages 組件的樣式，將容器位置改為固定，並增加 z-index 以改善顯示效果

--- src/components/ToastMessages.vue
+++ src/components/ToastMessages.vue
@@ -1,25 +1,40 @@
 <template>
-  <div aria-live="polite" aria-atomic="true" class="position-relative">
-    <div class="toast-container top-0 end-0 p-3">
-
-      <!-- Then put toasts within -->
-      <transition-group name="move" tag="div" class="toast-wrapper" mode="out-in">
-        <div v-for="(msg, key) in messages" :key="`toast-${key}`"
-         class="toast show toast-message" role="alert"
-          aria-live="assertive" aria-atomic="true">
-          <!-- Toast內容 -->
-          <div class="toast-header">
-            <span :class="`bg-${msg.style}`" class="d-inline-block p-2 rounded-circle me-2"></span>
-            <strong class="me-auto">{{msg.title}}</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"
-              @click="removeMessage(key)"></button>
-          </div>
-          <div class="toast-body" v-if="msg.content">
-            {{msg.content}}
-          </div>
+  <div
+    aria-live="polite"
+    aria-atomic="true"
+    class="position-fixed top-0 end-0 p-3"
+    style="z-index: 1055;"
+  >
+    <!-- Then put toasts within -->
+    <transition-group name="move" tag="div" class="toast-wrapper" mode="out-in">
+      <div
+        v-for="(msg, key) in messages"
+        :key="`toast-${key}`"
+        class="toast show toast-message"
+        role="alert"
+        aria-live="assertive"
+        aria-atomic="true"
+      >
+        <!-- Toast內容 -->
+        <div class="toast-header">
+          <span
+            :class="`bg-${msg.style}`"
+            class="d-inline-block p-2 rounded-circle me-2"
+          ></span>
+          <strong class="me-auto">{{msg.title}}</strong>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="toast"
+            aria-label="Close"
+            @click="removeMessage(key)"
+          ></button>
         </div>
-      </transition-group>
-    </div>
+        <div class="toast-body" v-if="msg.content">
+          {{msg.content}}
+        </div>
+      </div>
+    </transition-group>
   </div>
 </template>
 
@@ -35,7 +50,6 @@ export default {
     ...mapActions(useToastMessageStore, ['removeMessage']),
   },
 };
-
 </script>
 
 <style>
