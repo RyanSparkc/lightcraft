2bd550611e56d5a6256f10c2d8bb623f4fca48f0
forest079 <bluegx08@gmail.com>
2025-06-16 16:51:49 +0800

    Updates

--- assets/modalMixin-vLlVTyyO.js
+++
@@ -1,1 +0,0 @@
-import{M as o}from"./bootstrap.esm-UN3Qh_Ru.js";const d={methods:{openModal(){this.modal.show()},closeModal(){this.modal.hide()}},mounted(){this.modal=new o(this.$refs.modal,{backdrop:"static",keyboard:!1})}};export{d as m};
